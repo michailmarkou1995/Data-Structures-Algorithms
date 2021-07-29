f634fde7fbd0732364a2b2f07fa6eebf197c734b
michail <backtrackpower@gmail.com>
2021-07-29 18:11:24 +0300

    clean code

--- Familly_Tree_duck_maven/src/main/java/org/family/Family.java
+++ Familly_Tree_duck_maven/src/main/java/org/family/Family.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -40,20 +40,19 @@ public class Family {
     static List<List<String>> records2 = new ArrayList<>();
     static HashMap<String, String> childs = new HashMap<String, String>();
     //StringBuffer
-    static String they_are, they_are_not, they_have = "", root_p = null, husband, wife, last_name_husband, last_name_wife;
-    static boolean related_as_same_root = false, not_children_of_parent = false, isSiblings1 = false, isSiblings2 = false, not_related_as_same_root = false, incest = false, add_they_have=false;
+    static String they_are, they_have = "", root_p = null, husband, wife, last_name_husband, last_name_wife;
+    static boolean related_as_same_root = false, not_children_of_parent = false, isSiblings1 = false, isSiblings2 = false, not_related_as_same_root = false, incest = false, add_they_have = false;
     static Generation gen = new Generation();
 
     //This function reads the csv file and stores its content on an Arraylist, printing the result
     public static void readcsv(String path, int input, String FName1_originalCase, String FName2_originalCase) {
         String line = "";
         final String csvSplitBy = ",";
-        String FName1="", FName2="";
-        if (FName1_originalCase == null && FName2_originalCase == null)
-        {
-        }
-        else
-        {
+        String FName1 = "", FName2 = "";
+        if (FName1_originalCase == null && FName2_originalCase == null) {
+        } else {
+            assert FName1_originalCase != null;
+            assert FName2_originalCase != null;
             FName1 = FName1_originalCase.toLowerCase(Locale.ROOT);
             FName2 = FName2_originalCase.toLowerCase(Locale.ROOT);
         }
@@ -75,7 +74,7 @@ public class Family {
 
     //this function sorts the Arraylist alphabetically and then exports a sorted csv file
     private static void sortcsv() {
-        Collections.sort(myList, new Comparator<Generation>() {
+        myList.sort(new Comparator<Generation>() {
 
             //compares series of pairs of elements and based on result of compare it sees if elements from that pair should be swapped or not
             public int compare(Generation o1, Generation o2) {
@@ -85,14 +84,14 @@ public class Family {
             }
         });
         //loop that prints the sorted result
-        for (int i = 0; i < myList.size(); i++) {
-            System.out.println(myList.get(i));
+        for (Generation generation : myList) {
+            System.out.println(generation);
         }
         // Create csv
         FileWriter writer;
         try {
             //creating object writer with a path and charset format
-            writer = new FileWriter("resources/Duck_generated.csv", Charset.forName("utf-8"));
+            writer = new FileWriter("resources/Duck_generated.csv", StandardCharsets.UTF_8);
 
             //this loop uses Filewriter "write" function to print the result on the newly created csv file
             for (Generation s : myList) {
@@ -127,19 +126,19 @@ public class Family {
         readcsv(csvFile, input, null, null);
         FileWriter writerF;
         try {
-            writerF = new FileWriter("resources/toGraphViz.dot", Charset.forName("utf-8"));
+            writerF = new FileWriter("resources/toGraphViz.dot", StandardCharsets.UTF_8);
 
             writerF.write("digraph DUCK {\n");
             writerF.write("rankdir=LR;\n");
             writerF.write("size=\"8,5\"\n");
             writerF.write("node [shape = rectangle] [color=black];\n");
 
-            myList2.entrySet().forEach(entry -> {
-                System.out.println("\"" + entry.getValue().getName() + "\" -> \"" + entry.getValue().getChild() + "\" [label=\""
-                        + entry.getValue().getRelated() + "\"];");
+            myList2.forEach((key, value) -> {
+                System.out.println("\"" + value.getName() + "\" -> \"" + value.getChild() + "\" [label=\""
+                        + value.getRelated() + "\"];");
                 try {
-                    writerF.write("\"" + entry.getValue().getName() + "\" -> \"" + entry.getValue().getChild() + "\" [label=\""
-                            + entry.getValue().getRelated() + "\"];\n");
+                    writerF.write("\"" + value.getName() + "\" -> \"" + value.getChild() + "\" [label=\""
+                            + value.getRelated() + "\"];\n");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -158,94 +157,57 @@ public class Family {
         // Check if two Inputs have same parents
         for (Generation g : Generation.getObj) {
             String[] names_childs_equals = g.getChild().toLowerCase().split(" ");
-            for (int i = 0; i < names_childs_equals.length; i++) {
-                if (N1.equals(names_childs_equals[i])) {
+            for (String names_childs_equal : names_childs_equals) {
+                if (N1.equals(names_childs_equal)) {
                     isSiblings1 = true;
                 }
-                if (N2.equals(names_childs_equals[i]))
+                if (N2.equals(names_childs_equal))
                     isSiblings2 = true;
             }
         }
 
         // if they are something (2 inputs) meaning 3 cols how many childs have List?
         if (input == 3) {
-            //if (related_as_same_root) { ///////////old replaced with add_they_have
             if (add_they_have) {
                 AtomicInteger count = new AtomicInteger();
-                //System.out.print("\n" + they_are); ///////////old just commented out
                 if (!not_children_of_parent) {
-                    childs.entrySet().forEach(entryChild -> {
+                    childs.forEach((key, value) -> {
                         if (childs.size() == 1) {
-                            they_have += " and this is their child [" + entryChild.getValue() + "]";
+                            they_have += " and this is their child [" + value + "]";
                         } else {
                             count.getAndIncrement();
                             if (count.get() == 1)
-                                they_have += "and have these children's [" + entryChild.getValue();
+                                they_have += "and have these children's [" + value;
                             else if (count.get() != 1 && count.get() != childs.size())
-                                they_have += "," + entryChild.getValue();
-                            else they_have += "," + entryChild.getValue() + "]";
+                                they_have += "," + value;
+                            else they_have += "," + value + "]";
                         }
                     });
                 }
-            } //related_as_same_root = false;//else not_related_as_same_root = true; ///////////old replaced the second just commented out
+            }
         }
-////dux dumbella, grandma grandpa, julious jira, quackmore fanny, fanny poopy, louis ludwig, quackmore dumbella
- //scrooge dumbella
-          if (incest==false)
-          {
-              if (isSiblings1 && isSiblings2)
-                  System.out.println("\nSiblings in arms");
-              else if (!they_have.isEmpty())
-                  System.out.println("\n" + they_are + " (not related) " + they_have);
-              else
-                  System.out.println("\nnot related");
-          }
-          else if (incest==true)
-          {
-              System.out.println("\n" + they_are + " (related) " + they_have);
-          }
-          else
-          {
-              System.out.println("\nnot related");
-          }
 
-        ///////////OLD FUNCTIONALLITY RECOVER by uncommenting THE 10 / slashes
-//        if (((not_related_as_same_root == true == ((isSiblings1 == isSiblings2) == true)) != incest)) {//&& they_have.isEmpty()
-//            if ((!(isSiblings1 == isSiblings2) != incest)) {//&& !they_have.isEmpty()
-//                if (childs.size() == 1)
-//                    System.out.println(they_have);
-//                else if (((!not_children_of_parent && related_as_same_root) != incest) && they_have.isEmpty() == false)
-//                    System.out.println(" and have these children's [" + they_have + "]");
-//                else if ((!not_children_of_parent && related_as_same_root) != incest) {
-//                } else if (incest)
-//                    System.out.println(" They are also Siblings/Incest and have these children's [" + they_have + "]");
-//            } else
-//                System.out.println("Siblings in Arms");
-//        } else if ((!they_are.isEmpty() != !incest) /*&& they_have.isEmpty() == false*/) {
-//            if (childs.size() == 1)
-//                System.out.println(they_have);
-//            else if ((!not_children_of_parent && related_as_same_root) != incest)
-//                System.out.println(" and have these children's [" + they_have + "]");
-//        } else if (!they_are.isEmpty()  && they_have.isEmpty() == false) {
-//            String inc = "(related)";
-//            if (childs.size() == 1)
-//                if (!related_as_same_root)
-//                    System.out.println(they_have);
-//                else
-//                    System.out.println(they_have+ " " + inc);
-//            else if ((!not_children_of_parent && related_as_same_root))
-//                System.out.println(" and have these children's [" + they_have + "]");
-//        } else if (!they_are.isEmpty() && they_have.isEmpty() == true) {
-//        } else if (!they_are.isEmpty()) {
-//        } else System.out.println("They Are not Related");
+        if (!incest) {
+            if (isSiblings1 && isSiblings2)
+                System.out.println("\nSiblings in arms");
+            else if (!they_have.isEmpty())
+                System.out.println("\n" + they_are + " (not related) " + they_have);
+            else if (they_have.isEmpty())
+                System.out.println("\n" + they_are + " (not related)");
+            else
+                System.out.println("\nnot related");
+        } else if (incest) {
+            System.out.println("\n" + they_are + " (related) " + they_have);
+        } else {
+            System.out.println("\nnot related");
+        }
     }
 
     /* kane se olous osous no incest na leei no related + opoioi ine makrinoi na leei not related */
     private static void input_checking_scenario_Loop(int input, String FName1, String FName2, String[] allStrings) {
-        String[] decoy_strings=allStrings.clone();
-        String[] allStringsLC=decoy_strings;
+        String[] allStringsLC = allStrings.clone();
         for (int i = 0; i < allStrings.length; i++) {
-            allStringsLC[i]=allStrings[i].toLowerCase(Locale.ROOT); //str.substring(0,1).toUpperCase()
+            allStringsLC[i] = allStrings[i].toLowerCase(Locale.ROOT); //str.substring(0,1).toUpperCase()
         }
 
         // menu 1 print List of CSV after read
@@ -290,15 +252,21 @@ public class Family {
                     if (allStrings[1].equals("husband")) {
                         husband = allStrings[1];
                         String[] hus_name = allStrings[0].split(" ");
-                        for (int i = 0; i < hus_name.length; i++) {
-                            if (hus_name[i].equals("Duck")) blood_hus = true;
+                        for (String s : hus_name) {
+                            if (s.equals("Duck")) {
+                                blood_hus = true;
+                                break;
+                            }
                         }
                     }
                     if (allStrings[1].equals("wife")) {
                         wife = allStrings[1];
                         String[] hus_wife = allStrings[0].split(" ");
-                        for (int i = 0; i < hus_wife.length; i++) {
-                            if (hus_wife[i].equals("Duck")) blood_wife = true;
+                        for (String s : hus_wife) {
+                            if (s.equals("Duck")) {
+                                blood_wife = true;
+                                break;
+                            }
                         }
                     }
                     if (blood_hus == blood_wife) incest = true;
@@ -306,8 +274,7 @@ public class Family {
                     they_are = allStrings[0] + " is " + allStrings[1] + " of " + allStrings[2];
 
                     add_they_have = true;
-                    //related_as_same_root =true; ///////////old replaced with add_they_have
-                } //else they_are_not = "not related"; ///////////old replaced with add_they_have
+                }
             }
         }
         if (input == 4) {
@@ -323,31 +290,26 @@ public class Family {
 
     public static void main(String[] args) {
         //Creating the text menu
-        String csvFile = Objects.requireNonNull(new Family().getClass().getClassLoader().getResource("family.csv")).getPath();
+        String csvFile = Objects.requireNonNull(Family.class.getClassLoader().getResource("family.csv")).getPath();
         System.out.println("***This is the Duck family tree app***");
         System.out.println("***Main Menu***");
         try (Scanner menu = new Scanner(System.in)) {
             System.out.println("Press 1 to read the family members list \nPress 2 to create an alphabetically sorted family members list \nPress 3 to use the relationship app \nPress 4 to generate the .dot file");
             int input = menu.nextInt();
             switch (input) {
-                case 1:
-                    readcsv(csvFile, input, null, null);
-                    break;
-                case 2:
+                case 1 -> readcsv(csvFile, input, null, null);
+                case 2 -> {
                     //It is important to run readcsv() first, for sortcsv() to work
                     readcsv(csvFile, input, null, null);
                     System.out.println("\n");
                     sortcsv();
-                    break;
-                case 3:
-                    searchNameRelations(csvFile, input);
-                    break;
-                case 4:
+                }
+                case 3 -> searchNameRelations(csvFile, input);
+                case 4 -> {
                     myList.clear();
                     createDot(csvFile, input);
-                    break;
-                default:
-                    System.out.println("Please try again!");
+                }
+                default -> System.out.println("Please try again!");
             }
         } catch (Exception e) {
             System.out.println("Input Error ... Program Exits ...");
